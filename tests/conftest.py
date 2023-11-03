efbc7887028edb7458bfe8efb58be04ae3320522
Claudio <claudiocdadm@hotmail.com>
2023-11-03 10:05:15 -0300

    Criação do conftest.py e config.py

---
+++ tests/conftest.py
@@ -0,0 +1,71 @@
+import os
+import pytest
+from selenium import webdriver
+from .import config
+
+def pytest_addoption(parser):
+    parser.addoption(
+        '--baseurl',
+        action='store',
+        default='https://the-internet.herokuapp.com',
+        help='Url base da aplicacao alvo do teste'
+    )
+    parser.addoption(
+        '--host',
+        action='store',
+        default='saucelabs',
+        help='Onde vamos executar nossos testes: localhost ou saucelabs'
+    )
+    parser.addoption(
+        '--browser',
+        action='store',
+        default='chrome',
+        help='O nome do navegador utilizado nos testes'
+    )
+    parser.addoption(
+        '--browserversion',
+        action='store',
+        default='109.0.5414.168',
+        help='Versao do browser'
+    )
+    parser.addoption(
+        '--platform',
+        action='store',
+        default='Windows 8.1',
+        help='Sistema Operacional a ser utilizado durante os testes (apenas no saucelabs)'
+    )
+
+@pytest.fixture
+def driver(request):
+    config.baseurl = request.config.getoption('--baseurl')
+    config.host = request.config.getoption('--host')
+    config.browser = request.config.getoption('--browser')
+    config.browserversion = request.config.getoption('--browserversion')
+    config.platform = request.config.getoption('--platform')
+
+    if config.host == 'saucelabs':
+        test_name = request.node.name #nome do teste
+        capabilities = {
+            'browserName': config.browser,
+            'browserversion': config.browserversion,
+            'platformName': config.platform,
+            'sauce:options': {
+                'name': test_name
+            }
+        }
+        _credentials = os.environ['oauth-claudiocdadm-2d380'] + ':' + os.environ['6e87a4a4-a166-422f-95e3-b65f29cb08fd']
+        _url = 'http://' + _credentials + '@ondemand.us-west-1.saucelabs.com:443/wd/hub'
+        driver_ = webdriver.Remote(_url, capabilities)
+    else: # execução local / localhost
+        if config.browser == 'chrome':
+            _chromedriver = os.path.join(os.getcwd(), 'vendor', 'chromedriver')
+            if os.path.isfile(_chromedriver):
+                driver_ = webdriver.Chrome(_chromedriver)
+            else:
+                driver_ = webdriver.Chrome()
+        elif config.browser == 'firefox':
+            _geckodriver = os.path.join(os.getcwd(), 'vendor', 'geckodriver.exe')
+            if os.path.isfile(_geckodriver):
+                driver_ = webdriver.Firefox(_geckodriver)
+            else:
+                driver_ = webdriver.Firefox()
